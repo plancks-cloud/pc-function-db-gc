dfc91d63333c531755b02c28ae089d37e825a6ec
Captain Justin <just1689@gmail.com>
2018-03-02 08:16:50 +0200

    Stubbed out agnostic storage

--- index.js
+++ index.js
@@ -13,75 +13,107 @@ exports.handle = (req, res) => {
   if (req.method == "POST") {
     return SimpleHttpResponder.handlePost(req, res)
   }
-  if (req.method == "GET") {
-    return SimpleHttpResponder.handleGet(req, res)
-  }
+  // if (req.method == "GET") {
+  //   return SimpleHttpResponder.handleGet(req, res)
+  // }
 }
 
 class SimpleHttpResponder {
-  static handlePost(req, res) {
-    const key = datastore.key(["capacity", req.body.friendlyName])
-    const subEntity = [
-      {
-        name: 'capacity',
-        value: req.body.currentPlayers,
-        excludeFromIndexes: true,
-      },
-      {
-        name: 'maxPlayers',
-        value: req.body.maxPlayers,
-        excludeFromIndexes: true,
-      },
-      {
-        name: 'friendlyName',
-        value: req.body.friendlyName,
-        excludeFromIndexes: true,
-      },
-      {
-        name: 'joinUrl',
-        value: req.body.joinUrl,
-        excludeFromIndexes: true,
-      },
-      {
-        name: 'lastUpdate',
-        value: new Date().getTime(),
-      },
-    ]
 
-    const entity = {
-      key: key,
-      data: subEntity
+  static getField(obj, fieldName) {
+    return obj[fieldName]
+  }
+
+  static handlePost(req, res) {
+
+    /*
+    Expects:
+    body = {
+      collection: "collectionName"
+      index: "idFieldName"
+      rows = [
+        {field: value},
+        {}
+      ]
+    }
+    */
+
+    for (const row of req.body.rows) {
+      const key = datastore.key([req.body.collection, SimpleHttpResponder.getField(row, req.body.index)])
+      const subEntity = []
+      for (var r in row) {
+        const newObject = {
+          name: r,
+          value: SimpleHttpResponder.getField(row, r),
+          excludeFromIndexes: true,
+        }
+      }
+      subEntity.push(newObject)
+
+      //   {
+      //     name: 'capacity',
+      //     value: req.body.currentPlayers,
+      //     excludeFromIndexes: true,
+      //   },
+      //   {
+      //     name: 'maxPlayers',
+      //     value: req.body.maxPlayers,
+      //     excludeFromIndexes: true,
+      //   },
+      //   {
+      //     name: 'friendlyName',
+      //     value: req.body.friendlyName,
+      //     excludeFromIndexes: true,
+      //   },
+      //   {
+      //     name: 'joinUrl',
+      //     value: req.body.joinUrl,
+      //     excludeFromIndexes: true,
+      //   },
+      //   {
+      //     name: 'lastUpdate',
+      //     value: new Date().getTime(),
+      //   },
+      // ]
+
+      const entity = {
+        key: key,
+        data: subEntity
+      }
+
+      return datastore.save(entity)
+        .then(() => {
+          res.writeHead(200, {
+            "Content-type": "Application/json"
+          })
+          res.end(OK_JSON)
+          return Promise.resolve()
+        })
+        .catch((err) => {
+          console.error(err)
+          res.status(500).send(err.message)
+        })
+
     }
 
-    return datastore.save(entity)
-      .then(() => {
-        res.writeHead(200, {
-          "Content-type": "Application/json"
-        })
-        res.end(OK_JSON)
-        return Promise.resolve()
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send(err.message)
-      })
+
   }
 
-  static handleGet(req, res) {
-    const query = datastore
-      .createQuery('capacity')
-    datastore.runQuery(query)
-      .then(results => {
-        res.writeHead(200, {
-          "Content-type": "Application/json"
-        })
-        res.end(JSON.stringify(results[0]))
-        return Promise.resolve()
-      })
-      .catch(erro => {
-        res.status(500).send(erro)
-        return Promise.resolve()
-      })
-  }
-  
+  // static handleGet(req, res) {
+  //   const query = datastore
+  //     .createQuery('capacity')
+  //   datastore.runQuery(query)
+  //     .then(results => {
+  //       res.writeHead(200, {
+  //         "Content-type": "Application/json"
+  //       })
+  //       res.end(JSON.stringify(results[0]))
+  //       return Promise.resolve()
+  //     })
+  //     .catch(erro => {
+  //       res.status(500).send(erro)
+  //       return Promise.resolve()
+  //     })
+  // }
+
 }
